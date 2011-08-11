c665004bf4b61ad62aac58e82ad39f5846125598
Michael Mulich <mrm41@psu.edu>
2011-08-11 10:50:10 -0400

    Pull in selected chicklets from the settings.

--- collective/addthis/addthis.py
+++ collective/addthis/addthis.py
@@ -1,19 +1,29 @@
+from zope.component import getUtility
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
 from plone.app.layout.viewlets import common
+from zope.schema.interfaces import IVocabularyFactory
 
 
 class AddThisViewlet(common.ViewletBase):
     """ AddThis viewlet """
-    
+
     index = ViewPageTemplateFile('addthis.pt')
+
+    @property
+    def _settings(self):
+        pprop = getToolByName(self.context, 'portal_properties')
+        return pprop.addthis_properties
+
+    @property
+    def chicklets(self):
+        vocab = getUtility(IVocabularyFactory, name="AddThis Social Media")
+        results = [term for term in vocab(self.context)
+                   if term.value in self._settings.addthis_chicklets]
+        return results
 
     def getAddThisURL(self):
         """ Returns URL to AddThis service. If that isn't specified we'll return random URL I got from
         AddThis.com when this addon was developed. 
         """
-        
-        pprop = getToolByName(self, 'portal_properties')
-        addthis = pprop.addthis_properties
-        
-        return addthis.addthis_url or "http://www.addthis.com/bookmark.php?v=250&amp;username=xa-4b7fc6a9319846fd"
+        return self._settings.addthis_url or "http://www.addthis.com/bookmark.php?v=250&amp;username=xa-4b7fc6a9319846fd"
